90789148ce4f834079ec89139c916683da5712e6
mstrucka <mstrucka@gmail.com>
2022-10-18 19:23:12 +0200

    updated db on server, joins not working

--- graphql/index.ts
+++ graphql/index.ts
@@ -4,7 +4,7 @@ import Database, { Database as DB} from "better-sqlite3";
 import fetch from 'cross-fetch';
 
 const db_connect = async () => {
-    const db_file = await fetch('https://keablob.blob.core.windows.net/products/products.db');
+    const db_file = await fetch('https://keablob.blob.core.windows.net/products/products_final.db');
     const db_db = await db_file.arrayBuffer();
     const db_buff = Buffer.from(db_db);
     const db = new Database(db_buff);
@@ -27,7 +27,23 @@ const typeDefs = gql`
         price: Float
         link: String
         overall_rating: Float
+        product_image: ProductImage
+        product_info: ProductAdditionalInfo
     }
+
+    type ProductImage {
+        product_id: ID!
+        image_url: String
+        alt_text: String
+        additional_info: String
+    }
+
+    type ProductAdditionalInfo {
+        product_id: ID!
+        choices: String
+        additional_info: String
+    }
+
     type Query {
         getProduct(id: ID!): Product
         getProducts: [Product]
@@ -40,7 +56,9 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         getProduct: (_, args) => db.prepare(
-            `SELECT * FROM products
+            `SELECT * 
+            FROM products
+            FULL OUTER JOIN product_images ON products.id=product_images.product_id
             WHERE id = ?`
         ).get(args.id),
         
@@ -49,6 +67,7 @@ const resolvers = {
         ).all(),
         
         getProductsBySearchTerm: (_,args) => {
+            // const db = new Database('foobar.db', { verbose: console.log });  
             return db.prepare(
             `SELECT * FROM products
             WHERE product_name LIKE '%${args.term}%' OR product_sub_title LIKE '%${args.term}%' OR product_description LIKE '%${args.term}%'`
